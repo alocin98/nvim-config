4ec67a8634b95a5eeb659ddf363a30a104ac45ba
nicolas-98 <nicolas.mueller@interdiscount.ch>
2023-11-15 21:42:35 +0100

    fix

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -20,7 +20,7 @@ vim.keymap.set("n", "<leader>svwm", function()
 end)
 
 -- Nicolas great remaps
-vim.keymap.set("nt", "<C-h>", "<C-W>h")
+vim.keymap.set("n", "<C-h>", "<C-W>h")
 vim.keymap.set("n", "<C-l>", "<C-W>l")
 vim.keymap.set("n", "<C-j>", "<C-W>j")
 vim.keymap.set("n", "<C-k>", "<C-W>k")
