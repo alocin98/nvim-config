9ccf3229bef370cf6b7ac3dfc5cbc3ec9843185f
Nicolas Müller <nicolas.mueller2@gmail.com>
2026-08-13 13:14:21 +0200

    fix: telescope error

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,22 +1,25 @@
-require 'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or "all"
-    ensure_installed = { "vimdoc", "javascript", "typescript", "c", "lua", "rust" },
+local treesitter = require("nvim-treesitter")
 
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
-
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
-
-    highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-    },
+local ensure_installed = {
+    "vimdoc",
+    "javascript",
+    "typescript",
+    "c",
+    "lua",
+    "rust",
+    "markdown",
+    "markdown_inline",
 }
+
+treesitter.setup()
+treesitter.install(ensure_installed)
+
+local highlight_group = vim.api.nvim_create_augroup("alocin98-treesitter-highlight", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+    group = highlight_group,
+    pattern = { "vimdoc", "javascript", "typescript", "c", "lua", "rust", "markdown" },
+    callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+    end,
+})
--- lua/alocin98/plugins.lua
+++ lua/alocin98/plugins.lua
@@ -8,7 +8,8 @@ return {
     },
     {
         "nvim-treesitter/nvim-treesitter",
-        branch = "master",
+        branch = "main",
+        lazy = false,
         build = ":TSUpdate",
     },
     {
@@ -30,7 +31,7 @@ return {
     -- Telescope
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.8",
+        tag = "v0.2.2",
         dependencies = { "nvim-lua/plenary.nvim" },
     },
     {
